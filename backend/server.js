40302ee624692ebe35796a08e59768b44a47d1db
bonsurdev-code <bonsurdev@gmail.com>
2026-05-04 20:21:09 +0800

    dep #3

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,15 @@ import publicRoutes from './routes/public.routes.js';
 const app = express()
 const PORT = process.env.PORT || 5000
 
-// app.use(cors({
-//     origin: 'https://acadara.netlify.app',
-//     credentials: true
-// }))
-app.use(express.static("public"));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://acadara-test.netlify.app',
     credentials: true
 }))
+// app.use(express.static("public"));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true
+// }))
 app.use(cookieParser())
 app.use(express.json())
 app.use(morgan("dev"))
