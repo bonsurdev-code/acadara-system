2e2d2f66102827048b7ac81523ee5fc65376261a
kenbonzaiii <esmenakenneth33@gmail.com>
2026-08-31 23:38:55 +0800

    Refactor mailer configuration to use SMTP settings for Gmail

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -2,11 +2,14 @@ import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  family: 4,
 });
 
 transporter.verify((error, success) => {
